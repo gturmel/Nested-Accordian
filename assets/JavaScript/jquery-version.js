d5aebcd05875f99c6a30f1c01794afb84a4355aa
Greg Turmel <gregturmel@gmail.com>
2016-02-16 21:25:17 -0500

    gonna learn some jQuery now

--- assets/JavaScript/jquery-version.js
+++ assets/JavaScript/jquery-version.js
@@ -1,15 +1,37 @@
 $(function(){
    'use strict'
+
+// this is the accordion effect for the top level things
+
 $("#headOne").on("click", function(){
    $("#one").toggleClass("hidden");
+   $("#two").addClass("hidden");
+   $("#three").addClass("hidden");
+
 });
+
 $("#headTwo").on("click", function(){
-   $("#two").toggleClass("hidden");
+   $("article").toggleClass("hidden");
+   $("#one").addClass("hidden");
+   $("#three").addClass("hidden");
+
 });
+
 $("#headThree").on("click", function(){
-   $("#three").toggleClass("hidden");
-   
+   $("article").toggleClass("hidden");
+   $("#two").addClass("hidden");
+   $("#one ").addClass("hidden");
+
 });
+
+// hover effects for the header
+
+$("header").hover(function(){
+   $(this).css("background","red"),
+   $(this).css("background","#eee")
+});
+
+
 });
 
 
